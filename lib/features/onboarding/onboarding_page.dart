79b1930d88f30d633e6b43584cf18a66b098804a
mrcapel <mrcapeloficial@gmail.com>
2024-05-04 22:04:51 -0300

    novo commit

--- lib/features/onboarding/onboarding_page.dart
+++ lib/features/onboarding/onboarding_page.dart
@@ -4,6 +4,8 @@ import 'package:flutter/material.dart';
 import 'package:app_financy/common/constants/app_colors.dart';
 import 'package:app_financy/common/constants/app_text_styles.dart';
 import 'package:app_financy/common/widgets/primary_button.dart';
+import 'package:app_financy/features/usuario/usuario.dart';
+
 
 class OnboardingPage extends StatelessWidget {
   const OnboardingPage({Key? key}) : super(key: key);
@@ -11,6 +13,9 @@ class OnboardingPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text('Tela Inicial'),
+      ),
       body: Align(
         child: Column(
           children: [
@@ -48,6 +53,15 @@ class OnboardingPage extends StatelessWidget {
                 bottom: 16.0,
               ),
               child: CustomTextButton(),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => ProfileScreen()),
+                );
+              },
+              child: Text('Perfil'),
             ),
           ],
         ),
